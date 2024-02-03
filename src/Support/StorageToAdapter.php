509595e0457199be38e0653eb2bf30e26b4c39c3
as247 <as247@vui360.com>
2024-02-04 02:26:21 +0700

    Fix error on low than 3.2.20 flysystem

--- src/Support/StorageToAdapter.php
+++ src/Support/StorageToAdapter.php
@@ -243,7 +243,16 @@ trait StorageToAdapter
 	protected function convertConfig(Config $config=null)
 	{
         if($config){
-            return new \As247\CloudStorages\Support\Config($config->toArray());
+            if(method_exists($config,'toArray')) {
+                return new \As247\CloudStorages\Support\Config($config->toArray());
+            }else{
+                return new \As247\CloudStorages\Support\Config([
+                    'copy_destination_same_as_source'=>$config->get('copy_destination_same_as_source'),
+                    'move_destination_same_as_source'=>$config->get('move_destination_same_as_source'),
+                    'visibility'=>$config->get('visibility'),
+                    'directory_visibility'=>$config->get('directory_visibility'),
+                ]);
+            }
         }
 		return new \As247\CloudStorages\Support\Config();
 	}
